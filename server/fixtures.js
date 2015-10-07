cb0a064e053808a967ba09326d49f8c5c69b7dbb
tgoldenberg <thomasagoldenberg@gmail.com>
2015-10-07 13:13:04 -0400

    modify fixture data

--- server/fixtures.js
+++ server/fixtures.js
@@ -2,61 +2,71 @@ if (Messages.find().count() === 0) {
   Messages.insert({
     message: "Hello world",
     author: "Tom",
-    imageUrl: "",
+    teamId: "sous",
+    imageUrl: "https://lh3.googleusercontent.com/-8e1m8YBYSe0/AAAAAAAAAAI/AAAAAAAAAAA/jRTl40sO4fM/photo.jpg",
     createdAt: new Date()
   });
   Messages.insert({
     message: "Hey Tom, what's up?",
     author: "Harry",
-    imageUrl: "",
+    teamId: "sous",
+    imageUrl: "https://lh3.googleusercontent.com/-8e1m8YBYSe0/AAAAAAAAAAI/AAAAAAAAAAA/jRTl40sO4fM/photo.jpg",
     createdAt: new Date()
   });
   Messages.insert({
     message: "Hey guys from Cleveland",
     author: "Ilya",
-    imageUrl: "",
+    teamId: "sous",
+    imageUrl: "https://lh3.googleusercontent.com/-8e1m8YBYSe0/AAAAAAAAAAI/AAAAAAAAAAA/jRTl40sO4fM/photo.jpg",
     createdAt: new Date()
   });
   Messages.insert({
     message: "Hey it's me, Don",
     author: "Don",
-    imageUrl: "",
+    teamId: "sous",
+    imageUrl: "https://lh3.googleusercontent.com/-8e1m8YBYSe0/AAAAAAAAAAI/AAAAAAAAAAA/jRTl40sO4fM/photo.jpg",
     createdAt: new Date()
   });
   Messages.insert({
     message: "Hey Don",
     author: "Brian",
-    imageUrl: "",
+    teamId: "sous",
+    imageUrl: "https://lh3.googleusercontent.com/-8e1m8YBYSe0/AAAAAAAAAAI/AAAAAAAAAAA/jRTl40sO4fM/photo.jpg",
     createdAt: new Date()
   });
   Messages.insert({
     message: "Hi Brian",
     author: "Tom",
-    imageUrl: "",
+    teamId: "sous",
+    imageUrl: "https://lh3.googleusercontent.com/-8e1m8YBYSe0/AAAAAAAAAAI/AAAAAAAAAAA/jRTl40sO4fM/photo.jpg",
     createdAt: new Date()
   });
   Messages.insert({
     message: "Hey guys, Netflix and chill?",
     author: "Harry",
-    imageUrl: "",
+    teamId: "sous",
+    imageUrl: "https://lh3.googleusercontent.com/-8e1m8YBYSe0/AAAAAAAAAAI/AAAAAAAAAAA/jRTl40sO4fM/photo.jpg",
     createdAt: new Date()
   });
   Messages.insert({
     message: "What?",
     author: "Ilya",
-    imageUrl: "",
+    teamId: "sous",
+    imageUrl: "https://lh3.googleusercontent.com/-8e1m8YBYSe0/AAAAAAAAAAI/AAAAAAAAAAA/jRTl40sO4fM/photo.jpg",
     createdAt: new Date()
   });
   Messages.insert({
     message: "I don't know",
     author: "Don",
-    imageUrl: "",
+    teamId: "sous",
+    imageUrl: "https://lh3.googleusercontent.com/-8e1m8YBYSe0/AAAAAAAAAAI/AAAAAAAAAAA/jRTl40sO4fM/photo.jpg",
     createdAt: new Date()
   });
   Messages.insert({
     message: "Bye-bye",
     author: "Brian",
-    imageUrl: "",
+    teamId: "sous",
+    imageUrl: "https://lh3.googleusercontent.com/-8e1m8YBYSe0/AAAAAAAAAAI/AAAAAAAAAAA/jRTl40sO4fM/photo.jpg",
     createdAt: new Date()
   });
 }
