62355665bc320da599af203c06b50fe9b2911ca3
tgoldenberg <thomasagoldenberg@gmail.com>
2015-10-08 12:02:46 -0400

    implement stations as a Meteor collection

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,6 @@
 Meteor.publish('messages', function(teamKey) {
   return Messages.find({teamKey: teamKey});
 });
+Meteor.publish('stations', function(teamKey) {
+  return Stations.find({teamKey: teamKey})
+});
