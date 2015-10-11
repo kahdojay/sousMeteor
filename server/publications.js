696d7eea24dc189f25342f83db0a7ee07e06c7c0
license2e <ilya@shindyapin.com>
2015-10-10 22:35:34 -0400

    Add publish to user updates on restricted channel, Remove insecure user fields from being published.
    Signed-off-by: license2e <ilya@shindyapin.com>

--- server/publications.js
+++ server/publications.js
@@ -7,3 +7,20 @@ Meteor.publish('stations', function(teamKey) {
 Meteor.publish('purveyors', function(teamKey) {
   return Purveyors.find({teamKey: teamKey});
 });
+Meteor.publish('restricted', function(phoneNumber) {
+  //TODO: check if phoneNumber is valid
+  Meteor.users.update({username: phoneNumber}, {
+    $set:{
+      smsSent: false,
+      smsVerified: false,
+      smsTokenCount: 0,
+      smsToken: null
+    }
+  });
+  return Meteor.users.find({username: phoneNumber},{fields: {
+    id: 1,
+    smsSent: 1,
+    smsVerified: 1,
+    smsTokenCount: 1
+  }});
+});
