9fd3d29d69f8a121ab8f700f4f9ad5f93cfd4359
license2e <ilya@shindyapin.com>
2015-11-03 13:44:14 -0500

    Add teams-users publication channel
    Signed-off-by: license2e <ilya@shindyapin.com>

--- server/publications.js
+++ server/publications.js
@@ -6,6 +6,29 @@ Meteor.publish('messages', function(teamIds) {
 Meteor.publish('teams', function(userId) {
   return Teams.find({users: {$in: [userId]}});
 });
+Meteor.publish('teams-users', function(teamIds){
+  var teamUsers = Teams.find({_id: {$in: teamIds}}, {
+    fields: {
+      users: 1
+    }
+  }).fetch();
+  var userIds = {}
+  teamUsers.forEach(function(team){
+    team.users.forEach(function(userId){
+      userIds[userId] = true;
+    }.bind(this))
+  }.bind(this))
+  return Meteor.users.find({_id: {$in: Object.keys(userIds)}}, {
+    fields: {
+      firstName: 1,
+      lastName: 1,
+      username: 1,
+      smsToken: 0,
+      password: 0,
+      services: 0
+    }
+  });
+});
 Meteor.publish('purveyors', function(teamId) {
   return Purveyors.find();
   // TODO: limit by teamId
@@ -25,8 +48,8 @@ Meteor.publish('errors', function(userId) {
   return Errors.find({userId: userId});
 });
 Meteor.publish('restricted', function(phoneNumber) {
-  var users = Meteor.users.find({username: phoneNumber},
-    {fields: {
+  var users = Meteor.users.find({username: phoneNumber},{
+    fields: {
       smsToken: 0,
       password: 0,
       services: 0
