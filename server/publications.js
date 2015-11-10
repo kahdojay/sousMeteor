d7ee671fede31c9e40f2ca94b6ff81f9463736a3
license2e <ilya@shindyapin.com>
2015-11-09 21:25:23 -0500

    Change the team members and messages to exclude superUsers
    Signed-off-by: license2e <ilya@shindyapin.com>

--- server/publications.js
+++ server/publications.js
@@ -1,12 +1,4 @@
-Meteor.publish('messages', function(teamIds) {
-  // console.log('TEAM IDS', teamIds)
-  //TODO limit 20 for each teamId in teamIds
-  return Messages.find({teamId: {$in: teamIds}}, {sort: {createdAt: -1}, limit:20});
-});
-Meteor.publish('teams', function(userId) {
-  return Teams.find({users: {$in: [userId]}});
-});
-Meteor.publish('teams-users', function(teamIds){
+function getTeamUsers(teamIds) {
   var teamUsers = Teams.find({_id: {$in: teamIds}}, {
     fields: {
       users: 1
@@ -17,15 +9,43 @@ Meteor.publish('teams-users', function(teamIds){
     team.users.forEach(function(userId){
       userIds[userId] = true;
     }.bind(this))
-  }.bind(this))
-  return Meteor.users.find({_id: {$in: Object.keys(userIds)}}, {
+  }.bind(this));
+  return userIds;
+}
+
+Meteor.publish('messages', function(teamIds) {
+  var teamUserIds = getTeamUsers(teamIds);
+  var filteredUserIds = Meteor.users.find({
+    _id: {$in: Object.keys(teamUserIds)},
+    superUser: false
+  }, {
+    fields: {
+      _id: 1,
+    }
+  }).fetch();
+  // console.log('TEAM IDS', teamIds)
+  //TODO limit 20 for each teamId in teamIds
+  var userIds = filteredUserIds.map(function(user){
+    return user._id;
+  })
+  return Messages.find({userId: {$in: userIds}}, {sort: {createdAt: -1}, limit:20});
+}.bind(this));
+Meteor.publish('teams', function(userId) {
+  return Teams.find({users: {$in: [userId]}});
+});
+Meteor.publish('teams-users', function(teamIds){
+  var teamUserIds = getTeamUsers(teamIds);
+  return Meteor.users.find({
+    _id: {$in: Object.keys(teamUserIds)},
+    superUser: false
+  }, {
     fields: {
       firstName: 1,
       lastName: 1,
       username: 1
     }
   });
-});
+}.bind(this));
 Meteor.publish('purveyors', function(teamId) {
   return Purveyors.find();
   // TODO: limit by teamId
