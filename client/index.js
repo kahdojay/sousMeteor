84bf57d0e087ea4806fdb56bd8997fb0149d1d01
license2e <ilya@shindyapin.com>
2015-11-21 12:08:30 -0500

    Added UI to the meteor app with version and build numbers
    Signed-off-by: license2e <ilya@shindyapin.com>

---
+++ client/index.js
@@ -0,0 +1,19 @@
+if(Meteor.isClient){
+  Session.setDefault('buildInfo', {});
+
+  Template.footer.helpers({
+    copyrightYear: function() {
+      return (new Date()).getFullYear()
+    },
+    buildInfo: function(){
+      return Session.get('buildInfo');
+    }
+  });
+
+  Template.footer.onCreated(function(){
+    var buildInfo = Meteor.call('getBuildInfo', function(err, buildInfo){
+      Session.set('buildInfo', buildInfo);
+    });
+  })
+
+}
