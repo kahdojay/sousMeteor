db69f17d9339827eb03eac3ec840d0d3fe03258d
license2e <ilya@shindyapin.com>
2016-03-02 10:38:47 -0700

    Fix exceptions for sms login, phaxio and product updates
    Signed-off-by: license2e <ilya@shindyapin.com>

--- server/fax.js
+++ server/fax.js
@@ -1,19 +1,23 @@
 Meteor.methods({
   faxOrder: function(options) {
     log.debug('sending Phaxio to: ', options.number)
-    var Phaxio = Npm.require('phaxio');
-    var phaxio = new Phaxio(Meteor.settings.PHAXIO.KEY, Meteor.settings.PHAXIO.SECRET)
-    var cb = function(err, data) {
-      if (err) {
-        log.debug('PHAXIO ERROR: ', err)
-      } else {
-        log.debug('PHAXIO DATA: ', data)
+    if(options.number){
+      var Phaxio = Npm.require('phaxio');
+      var phaxio = new Phaxio(Meteor.settings.PHAXIO.KEY, Meteor.settings.PHAXIO.SECRET)
+      var cb = function(err, data) {
+        if (err) {
+          log.error('PHAXIO SEND ERROR: ', err)
+        } else {
+          log.debug('PHAXIO SEND DATA: ', data)
+        }
       }
+      phaxio.sendFax({
+        to: options.number,
+        string_data: options.text,
+        string_data_type: 'text',
+      }, cb)
+    } else {
+      log.error('PHAXIO ERROR: Missing to number.')
     }
-    phaxio.sendFax({
-      to: options.number,
-      string_data: options.text,
-      string_data_type: 'text',
-    }, cb)
   }
 })
