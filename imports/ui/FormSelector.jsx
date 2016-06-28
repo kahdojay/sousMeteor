884768994f45418349fd2ef36e98ede7e72b4e89
Don J <kahdojay@gmail.com>
2016-06-27 22:39:03 -0400

    Implement AddToUsersTeams.

---
+++ imports/ui/FormSelector.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Meteor } from 'meteor/meteor'
+
+export default class FormSelector extends Component {
+  render() {
+    return (
+      <div className="dropdown">
+        <select 
+          onChange={this.props.switchForm}
+        >
+          <option value="addToTeams">Add To Teams</option> 
+          <option value="addToUsersTeams">Add To Another User's Teams</option>
+        </select>
+      </div>
+    )
+  }
+}
