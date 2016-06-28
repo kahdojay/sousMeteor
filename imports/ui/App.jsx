6eb98933dfd27cbe2c35286be962535db6239214
Don J <kahdojay@gmail.com>
2016-06-27 21:43:21 -0400

    Implement the most basic auth ever.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,23 +2,37 @@ import React, { Component } from 'react'
 import { createContainer } from 'meteor/react-meteor-data'
 
 import AddUserToTeamsForm from './AddUserToTeamsForm'
+import AuthForm from './AuthForm'
 
 class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      importType: 'addToTeams'
+      importType: 'addToTeams',
+      authenticated: false
     }
   }
 
+  submitPass(pass) {
+    Meteor.call('submitPass', pass, (err, res) => {
+      if (res === true) {
+        this.setState({ authenticated: true })
+      }
+    })
+  }
+
   render() {
+    var ShowForm = this.state.authenticated ? AddUserToTeamsForm : AuthForm
+
     return (
       <div className="container">
         <header>
           <h1>Sous</h1>
         </header>
-        <AddUserToTeamsForm/>
+        <ShowForm
+          submitPass={this.submitPass.bind(this)}
+        />
       </div>
     )
   }
