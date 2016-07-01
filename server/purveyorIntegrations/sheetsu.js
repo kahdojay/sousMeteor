a7ea26f6fb6a25f1daa4a089681c5396eca78e03
Don J <kahdojay@gmail.com>
2016-07-01 14:46:53 -0400

    Update sheetsu integration per HVM requests.

--- server/purveyorIntegrations/sheetsu.js
+++ server/purveyorIntegrations/sheetsu.js
@@ -4,26 +4,29 @@ if(Meteor.isServer){
     uploadOrderToSheetsu: function(endpoint, options) {
       // HVM ships to Bricolage only on Tuesdays, Mon 6PM cutoff
       log.debug('SENDING ORDER TO SHEETSU: ', endpoint, options.orderId)
-      var orderDate = moment(options.orderDate)
+      var orderDate = options.orderDate ? moment(options.orderDate) : moment()
       var orderCutoff = moment().startOf('week').add(1, 'days').add(18,'hours')
       var thisWeeksTues = moment().startOf('week').add(2, 'days')
       var nextWeeksTues = moment().startOf('week').add(9, 'days')
       var shipDate = orderDate.isBefore(orderCutoff) ? thisWeeksTues : nextWeeksTues
 
       var productRows = []
+      let productIdx = 0
       options.orderProductList.forEach(function(product) {
       log.debug('QUEUEING PRODUCT FOR SHEETSU: ', product)
         productRows.push({
-          "External ID": options.orderRef || 'n/a',
-          "Ship Date": shipDate.format('ddd M/D'),
-          "Customer ID": options.purveyor.customerNumber || 'n/a',
-          "Customer": options.team.name || 'n/a',
-          "Ordered Qty": `${product.quantity} ${product.unit}`,
-          "Item ID": product.sku,
-          "Item": product.name,
-          "Itm Notes": product.description,
-          "Order Date": orderDate.format('ddd M/D h:mm a'),
+          "lineid": productIdx,
+          "externalid": options.orderRef || 'n/a',
+          "shipdate": shipDate.format('ddd M/D'),
+          "customerid": options.purveyor.customerNumber || 'n/a',
+          "customer": options.team.name || 'n/a',
+          "orderedqty": `${product.quantity} ${product.unit}`,
+          "itemid": product.sku,
+          "item": product.name,
+          "itmnotes": product.description,
+          "orderdate": orderDate.format('MM/DD/YYYY'),
         })
+        productIdx ++
       })
       Meteor.http.post(endpoint, {
         data: {
