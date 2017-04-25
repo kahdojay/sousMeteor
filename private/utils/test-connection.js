c1d3ba58d547526ea4920d8ce25ec39bd8e9fc24
Ilya Shindyapin <ilya@shindyapin.com>
2017-04-25 14:49:41 -0700

    Fix test connection script
    Signed-off-by: Ilya Shindyapin <ilya@shindyapin.com>

--- private/utils/test-connection.js
+++ private/utils/test-connection.js
@@ -1,11 +1,38 @@
 var WebSocket = require('ws')
 var DDPClient = require('ddp-client')
-var ENDPOINT_WS = 'wss://sousmeteor-ippfnjlvqv.now.sh/websocket'
+var ENDPOINT_WS = 'wss://sousmeteor-opetrklpxc.now.sh/websocket'
 
+var pingCount = 0;
 var ddpClient = new DDPClient({url: ENDPOINT_WS, autoReconnect : false, maintainCollections : false, socketConstructor: WebSocket})
-ddpClient.on("message", function(){ console.log("MSG FUNC: ", arguments); })
-ddpClient.connect(function(error, wasReconnect){ console.log("CONN FUNC: ERROR: ", error, " WAS RECONNECT: ", wasReconnect); })
+ddpClient.on("message", function(message){
+  const res = JSON.parse(message);
+  console.log("MSG FUNC: ", res.msg);
 
-ddpClient.call('💓', [])
-ddpClient.call('getUserByPhoneNumber', ['5623105753'])
-ddpClient.close()
+  if (res.msg === 'connected') {
+    ddpClient.call('💓', [])
+    ddpClient.call('getUserByPhoneNumber', ['8067892921'])
+    ddpClient.call('getUserByPhoneNumber', ['5623105753'])
+    ddpClient.call('getSettingsConfig', [])
+    ddpClient.call('getBuildInfo', [])
+  }
+
+  if (res.msg === 'result') {
+    console.log("\n\n", JSON.stringify(res.result, null, 4));
+    pingCount = 0;
+  }
+
+  if (res.msg === 'ping') {
+    pingCount++;
+  }
+
+  if (pingCount > 1) {
+    console.log('Closing the connection due to inactivity.')
+    ddpClient.close()
+  }
+
+})
+
+
+ddpClient.connect(function(error, wasReconnect){
+  console.log("CONN FUNC: ERROR: ", error, " WAS RECONNECT: ", wasReconnect);
+})
